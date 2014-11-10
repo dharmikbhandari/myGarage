bc1aa9cc110bacb2f110b804044c75543757d901
Dharmik <dharmik_bhandari@hotmail.com>
2014-11-11 00:36:38 +0530

    Added angularjs demo

---
+++ js/myGarage.js
@@ -0,0 +1,37 @@
+﻿var myGarageApp = angular.module('myGarage', ['ngRoute']);
+
+
+// configure routes
+myGarageApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'generalPages/home.html',
+            controller: 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'generalPages/about.html',
+            controller: 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'generalPages/contact.html',
+            controller: 'contactController'
+        });
+});
+
+myGarageApp.controller('mainController', function ($scope) {
+    $scope.message = 'Hello Everyone!';
+});
+
+myGarageApp.controller('aboutController', function ($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+myGarageApp.controller('contactController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
