8ba291d7a768f16f977204fa08cd171e0bd203ab
Dharmik <dharmik_bhandari@hotmail.com>
2015-02-17 23:24:26 +0530

    Material Design
    Material Design

--- js/myGarage.js
+++ js/myGarage.js
@@ -1,53 +1,56 @@
-﻿var myGarageApp = angular.module('myGarage', ['ngMaterial']);
+﻿var myGarageApp = angular.module('myGarage', ['ngMaterial', 'ngRoute', 'ngAnimate']);
 
 
-//// configure routes
-//myGarageApp.config(function ($routeProvider) {
-//    $routeProvider
+// configure routes
+myGarageApp.config(function ($routeProvider) {
+    $routeProvider
 
-//        /*****route for generalPages*****/
+        /*****route for generalPages*****/
 
-//        // route for the home page
-//        .when('/', {
-//            templateUrl: 'generalPages/home.html',
-//            controller: 'homeController'
-//        })
+        // route for the home page
+        .when('/home', {
+            templateUrl: 'generalPages/home.html',
+            controller: 'homeController'
+        })
 
-//        // route for the about page
-//        .when('/about', {
-//            templateUrl: 'generalPages/about.html',
-//            controller: 'aboutController'
-//        })
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'generalPages/about.html',
+            controller: 'aboutController'
+        })
 
-//        // route for the contact page
-//        .when('/contact', {
-//            templateUrl: 'generalPages/contact.html',
-//            controller: 'contactController'
-//        })
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'generalPages/contact.html',
+            controller: 'contactController'
+        })
 
 
 
-//        /***** route for Vehicle *****/
-//        .when('/vehicle/add', {
-//            templateUrl: 'vehicle/add.html',
-//            controller: 'vehicleAddController'
-//        })
-//        .when('/vehicle/list', {
-//            templateUrl: 'vehicle/list.html',
-//            controller: 'vehicleListController'
-//        })
+        /***** route for Vehicle *****/
+        .when('/vehicle/add', {
+            templateUrl: 'vehicle/add.html',
+            controller: 'vehicleAddController'
+        })
+        .when('/vehicle/list', {
+            templateUrl: 'vehicle/list.html',
+            controller: 'vehicleListController'
+        })
 
 
 
         
 
-//    ;
-//});
+    ;
+});
 
 
 /***** General Controller *****/
 myGarageApp.controller('mainController', function ($scope) {
     $scope.message = 'Welcome Saheb...!';
+    $scope.Enter = function () {
+        location.href = "#/vehicle/add";
+    };
 });
 
 myGarageApp.controller('homeController', function ($scope) {
