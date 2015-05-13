6b4d46f185bc30ed58943df95b0e2ac3ab5a1021
Dharmik <dharmik_bhandari@hotmail.com>
2015-05-13 12:41:51 +0530

    Basic Structure of DB

--- js/myGarage.js
+++ js/myGarage.js
@@ -1,4 +1,4 @@
-﻿var myGarageApp = angular.module('myGarage', ['ngMaterial', 'ngRoute', 'ngAnimate']);
+﻿var myGarageApp = angular.module('myGarage', ['ngMaterial', 'ngRoute', 'ngAnimate', 'ngCordova']);
 
 
 // configure routes
@@ -61,7 +61,61 @@ myGarageApp.run(function ($rootScope) {
     $rootScope.navigateTo = function (navigateUrl) {
         location.href = navigateUrl;
     };
+
+
+    /***** Database *****/
+    //http://stackoverflow.com/questions/26604952/a-simple-cordova-android-example-including-sqlite-read-write-and-search
+    // Wait for Cordova to load
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova is ready
+    function onDeviceReady() {
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(populateDB, errorCB, successCB);
+    }
+
+    // Populate the database
+    function populateDB(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name,number)');
+    }
+
+    // Transaction error callback
+    function errorCB(err) {
+        alert("Error processing SQL: " + err.code);
+    }
+    // Transaction success callback
+    function successCB() {
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(queryDB, errorCB);
+    }
+    // Query the database
+    function queryDB(tx) {
+        tx.executeSql('INSERT INTO DEMO (name,number) VALUES ("Dharmik","123")');
+        tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+    }
+    // Query the success callback
+    function querySuccess(tx, results) {
+        var tblText = '<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
+        var len = results.rows.length;
+        for (var i = 0; i < len; i++) {
+            var tmpArgs = results.rows.item(i).id + ",'" + results.rows.item(i).name
+                    + "','" + results.rows.item(i).number + "'";
+            tblText += '<tr onclick="goPopup(' + tmpArgs + ');"><td>' + results.rows.item(i).id + '</td><td>'
+                    + results.rows.item(i).name + '</td><td>' + results.rows.item(i).number + '</td></tr>';
+        }
+        tblText += "</table>";
+        alert(tblText);
+    }
+
+
+
+
 });
+
+
+
+
+
 
 /***** General Controller *****/
 myGarageApp.controller('mainController', function ($scope) {
